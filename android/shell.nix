3b7a208296c0cfb50b2e75d7983f72b69e01aed6
Nick Novitski <nick@expo.io>
2018-10-24 21:26:21 -0700

    add memory jvmopts

--- android/shell.nix
+++ android/shell.nix
@@ -1,6 +1,7 @@
 with (import ../. {});
 
 mkShell {
+  JVM_OPTS="-Xmx3200m";
   JAVA_HOME=openjdk8;
   ANDROID_HOME="${androidenv.ourSDK}/libexec";
   ANDROID_NDK_HOME="${androidenv.androidndk_10e}/libexec/android-ndk-r10e";
